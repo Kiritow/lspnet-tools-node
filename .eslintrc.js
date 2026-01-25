33e928abacfebbb8436652d736a9bd363dffdcc3
Kiritow <1362050620@qq.com>
2026-01-25 12:14:59 +0000

    more strict eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
         "prettier",
         "plugin:prettier/recommended",
     ],
